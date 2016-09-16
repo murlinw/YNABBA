3e1575d7bb699e79849f2e1bfc7883a6fda0205b
Murlin Wei <murlin@wei.home>
2016-09-16 19:40:10 -0400

    Declaring components in module as per angular 2 reqs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { CategoryComponent } from './category/category.component';
-import { CategoryGroupComponent} from './category-group/category-group.component';
+
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/app.component.html',
-    directives: [CategoryComponent, CategoryGroupComponent]
+    templateUrl: 'app/app.component.html'
 })
 export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { CategoryComponent } from './category/category.component';
+import { CategoryGroupComponent} from './category-group/category-group.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent],
-  bootstrap: [ AppComponent ]
+  declarations: [ AppComponent,  CategoryComponent, CategoryGroupComponent],
+  bootstrap: [ AppComponent]
 })
 export class AppModule { }
